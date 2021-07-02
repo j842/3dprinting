c8f069db64194eaa91dc832ec0e6ea2b34b66a12
John <j842@users.noreply.github.com>
2021-07-02 17:53:17 +1200

    Update 20210630 Griff Test.lua

--- 20210622 Slade Test/20210630 Griff Test.lua
+++ 20210622 Slade Test/20210630 Griff Test.lua
@@ -1,39 +1,41 @@
 -- Griff Namebadge
 -- https://3dp.rocks/lithophane/
 
-h=5
+package.path = package.path .. ";../common/?.lua"
+jshapes=require("jshapes")
 
-function centerobj(objtocenter)
-    local v=bbox(objtocenter):center()
-    objtocenter=translate(-v.x,-v.y,-v.z)*objtocenter
-    return objtocenter
-end
-
-f=font(Path..'/../ttf/StardosStencil-Bold.ttf')
-letters=rotate(0,180,0)*scale(2,2,10*h)*f:str('Griff',1)
-letters=centerobj(letters)
-bb=bbox(letters):extent()
-letters=translate(0,0,bb.z/2)*letters
-
-xy=v(bb.x+15,bb.y+15,0)
-
-enclosure=translate(0,0,0)*cube(xy.x,xy.y,xy.y)
-
-ramp=translate(-xy.x/2,xy.y/2,xy.y+h)*rotate(0,90,0)*cylinder(xy.y,xy.x)
-ramp=difference(enclosure,ramp)
-
-tag=difference(
-ramp,letters
-)
+h=20
+w=120
+t=2
+g=3
+e=2
 
 lithopane=load_centered(Path..'griffbw.jpegW99H100T3V4B2A0C0NS.stl')
-l=scale(xy.y/100)*lithopane
+l=scale(h/bbox(lithopane):extent().x)*lithopane
 le=bbox(l):extent()
-l=translate(-xy.x/2-le.x/2,0,le.z/2)*l
+l=translate(-w/2+le.x/2,0,le.z/2)*l
 l=union(mirror(v(1,0,0))*l,l)
 
-tag=union(tag,l)
+
+f=font(Path..'/../ttf/StardosStencil-Bold.ttf')
+letters=rotate(0,180,0)*f:str('Griff',1)
+
+-- scale text to h-2*g height.
+lscale=(h-2*g)/bbox(letters):extent().y
+letters=scale(lscale)*letters
+letters=jshapes.xycenter(letters)
+
+tag=translate(0,0,0)*cube(w-2*le.x,h,t)
+
+tag=difference(
+tag,letters
+)
+
+border=difference(cube(w,h,t),cube(w-e,h-e,t))
+
+tag=union({tag,l,border})
 
 tag=rotate(90,0,0)*tag
 
-emit(tag)
+set_brush_color (1,1,1,1)
+emit(tag,1)
