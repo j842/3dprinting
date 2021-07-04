b3cb3de58c86e18f51db5940e8e06f8d9247b999
John <j842@users.noreply.github.com>
2021-07-04 18:53:03 +1200

    DaxWax

--- 20210703 TracMap Nametags/20210703 tracmap tag test.lua
+++ 20210703 TracMap Nametags/20210703 tracmap tag test.lua
@@ -2,39 +2,10 @@ package.path = package.path .. ";../common/?.lua"
 jshapes=require("jshapes")
 jtags=require("jtags")
 
-function holder()
-    local g=jtags.gettagborder()
-    local ts=jtags.gettagsize()
-    local hs=v(ts.x+2*g,25,ts.z+g)
-    local h=cube(hs)
+-- when printing nametag:
+-- first layer 0.3mm, others 0.1mm
 
-    local feather=0.5
-    -- remove slot for tag
-    h=difference(h,translate(0,hs.y/2-ts.y/2-g,hs.z-ts.z)*cube(ts+v(feather,feather,0)))
-    -- remove material in front of slot so you can see tag
-    h=difference(h,translate(0,hs.y/2-ts.y/2,hs.z-ts.z+g)*cube(ts.x-2*g,ts.y,ts.z-g))
-    -- remove material behind slot so light can get to it
-    h=difference(h,translate(0,0,g)*cube(hs.x-2*g,hs.y-g-ts.y,hs.z-2*g))
-    -- make hole for led cable
-    h=difference(h,translate(-hs.x/2+16,-hs.y/2+g,g)*cube(10,2*g,2))
-
-    -- put some bracing behind slot.
-    h=union(h,translate(0,hs.y/2-ts.y/2-g-ts.y-feather/2,hs.z-ts.z)*cube(ts))
-    h=difference(h,translate(0,hs.y/2-ts.y/2-g-ts.y-feather/2,hs.z-ts.z+g)*cube(ts.x-2*g,ts.y,ts.z-2*g))
-
-    local stickout=g
-    tracmap=jshapes.xycenter(mirror(v(1,0,0))*rotate(180,X)*load(Path..'tracmap black.stl'))
-    tbox=bbox(tracmap):extent()
-    tracmap=difference(scale(0.95,0.95,g/tbox.z)*cube(tbox),tracmap)
-    tracmap=jshapes.xycenter(tracmap)
-    tbox=bbox(tracmap):extent()
-    tracmap=translate(0,-hs.y/2+tbox.y/2+g,hs.z-tbox.z+g)*tracmap
-    h=union(h,tracmap)
-
-
-    h=union(h,translate(0,hs.y/2-g-ts.y/2,g)*magnet('h1'))
-    return h
-end
+-- 20% infill is fine for both
 
 -- https://3dp.rocks/lithophane/
 -- Model settings:
@@ -43,13 +14,13 @@ end
 -- Border 2
 -- Thinnest layer 0.5
 -- Positive image
-text='Griff'
-image='griffbw.jpegW24H25T3V4B2A0C0NS.stl'
+text='DaxWax'
+image='_daxwax.jpgW25H25T3V4B2A0C0NS.stl'
 tag=jtags.nametag(text,image)
 
-hold=holder()
+hold=jtags.holder()
 
-debug=true
+debug=false
 if (debug) then
   tmatrix=snap(hold,'h1',tag,'t1')
   set_brush_color (1,1,1,1)
@@ -59,7 +30,7 @@ if (debug) then
 else
   emit(tag)
   hold=translate(0,bbox(hold):extent().y,0)*jshapes.xycenter(rotate(90,X)*hold)
-  emit(hold)
+--  emit(hold)
 end
 
 
--- 20210703 TracMap Nametags/jtags.lua
+++ 20210703 TracMap Nametags/jtags.lua
@@ -92,5 +92,41 @@ end
 -------------------------------------------------------
 
 
+function jtags.holder()
+  local g=jtags.gettagborder()
+  local ts=jtags.gettagsize()
+  local hs=v(ts.x+2*g,25,ts.z+g)
+  local h=cube(hs)
+
+  local feather=0.5
+  -- remove slot for tag
+  h=difference(h,translate(0,hs.y/2-ts.y/2-g,hs.z-ts.z)*cube(ts+v(feather,feather,0)))
+  -- remove material in front of slot so you can see tag
+  h=difference(h,translate(0,hs.y/2-ts.y/2,hs.z-ts.z+g)*cube(ts.x-2*g,ts.y,ts.z-g))
+  -- remove material behind slot so light can get to it
+  h=difference(h,translate(0,0,g)*cube(hs.x-2*g,hs.y-g-ts.y,hs.z-2*g))
+  -- make hole for led cable
+  h=difference(h,translate(-hs.x/2+16,-hs.y/2+g,g)*cube(10,2*g,2))
+
+  -- put some bracing behind slot.
+  h=union(h,translate(0,hs.y/2-ts.y/2-g-ts.y-feather/2,hs.z-ts.z)*cube(ts))
+  h=difference(h,translate(0,hs.y/2-ts.y/2-g-ts.y-feather/2,hs.z-ts.z+g)*cube(ts.x-2*g,ts.y,ts.z-2*g))
+
+  local stickout=g
+  tracmap=jshapes.xycenter(mirror(v(1,0,0))*rotate(180,X)*load(Path..'_tracmap black.stl'))
+  tbox=bbox(tracmap):extent()
+  tracmap=difference(scale(0.95,0.95,g/tbox.z)*cube(tbox),tracmap)
+  tracmap=jshapes.xycenter(tracmap)
+  tbox=bbox(tracmap):extent()
+  tracmap=translate(0,-hs.y/2+tbox.y/2+g,hs.z-tbox.z+g)*tracmap
+  h=union(h,tracmap)
+
+
+  h=union(h,translate(0,hs.y/2-g-ts.y/2,g)*magnet('h1'))
+  return h
+end
+-------------------------------------------------------
+
+
 return jtags
   
