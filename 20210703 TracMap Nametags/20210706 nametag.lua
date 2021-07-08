09a0a0de20c62b885cc44ed2f2adf7c10228f32a
John <j842@users.noreply.github.com>
2021-07-08 20:56:51 +1200

    Everyone says flat is bad...

--- 20210703 TracMap Nametags/20210706 nametag.lua
+++ 20210703 TracMap Nametags/20210706 nametag.lua
@@ -9,7 +9,8 @@
 set_setting_value('use_different_thickness_first_layer',true)
 set_setting_value('z_layer_height_first_layer_mm',0.3)
 set_setting_value('z_layer_height_mm',0.1)
-set_setting_value('gen_supports',true)
+--set_setting_value('gen_supports',true)
+set_setting_value('gen_supports',false)
 set_setting_value('infill_percentage_0',5)
 
 
@@ -20,9 +21,15 @@ set_setting_value('infill_percentage_0',5)
   -- Border 2
   -- Thinnest layer 0.5
   -- Positive image
-  text='GEOFF'
-  image='_geoff.jpgW25H24T3V4B2A0C0NS.stl'
+  text='MARK'
+  image='_mark.jpgW25H25T3V4B2A0C0NS.stl'
   tag=jtags2.nametag(text,image)
-  tag=rotate(90,Z)*tag
+  
+  -- orientate around the Y axis
+  --tag=rotate(90,Z)*tag
+  
+  -- orientate flat
+  tag=rotate(90,X)*tag
+
   emit(tag)
 
