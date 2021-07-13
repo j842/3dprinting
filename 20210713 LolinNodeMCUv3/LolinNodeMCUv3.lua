96c46bcc390c4b0a253c8a31b56cd836d5ba2464
John <j842@users.noreply.github.com>
2021-07-14 09:30:32 +1200

    Update LolinNodeMCUv3.lua

--- 20210713 LolinNodeMCUv3/LolinNodeMCUv3.lua
+++ 20210713 LolinNodeMCUv3/LolinNodeMCUv3.lua
@@ -1,10 +1,14 @@
 
 
+function lolinsize()
+  return v(31,57.5,1.5)
+end
+
 function lolinv3template()
 -- ESP-8266 Lolin NodeMCU v3 board
-local l=57.5
-local w=31
-local boardthickness=1
+local w=lolinsize().x
+local l=lolinsize().y
+local boardthickness=lolinsize().z
 local pinheight=9
 local clearance=4
 local musbh=3.5
@@ -20,7 +24,7 @@ board=union(board,musb)
 
 local esp8266=
   translate(0,-13.7,-musbh)*
-  cube(12,15,clearance)
+  cube(13,16,clearance)
 board=union(board,esp8266)
 
 local hole=cylinder(3/2,boardthickness)
@@ -32,10 +36,31 @@ board=difference(board,hole)
 return board
 end
 
+function lolinv3mount(h)
+local ls=lolinsize()
 
-x=rotate(180,X)*lolinv3template()
+local hole=union(
+  cylinder(3/2,h),
+  translate(0,0,h)*cylinder(2.8/2,ls.z+3)
+)
 
-y=load('NodeMcu_V3.stl')
+hole=translate(-ls.x/2+2.3,-ls.y/2+2.3,0)*hole
+hole=union(hole,mirror(v(1,0,0))*hole)
+hole=union(hole,mirror(v(0,1,0))*hole)
+return hole
+end
+
+
+mountheight=4
+mnt=lolinv3mount(mountheight)
+set_brush_color(1,0,1,0)
+emit(mnt,1)
+
+
+--x=rotate(180,X)*lolinv3template()
+x=translate(0,0,mountheight)*lolinv3template()
+
+--y=load('NodeMcu_V3.stl')
 --emit(y)
 
 set_brush_color (2,1,0,0)
