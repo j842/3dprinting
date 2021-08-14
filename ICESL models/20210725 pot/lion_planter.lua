7ed10d03607c9d7fdd95c40ad7113f40663e382e
John <j842@users.noreply.github.com>
2021-08-14 15:23:47 +1200

    Update lion_planter.lua
    BROKEN

--- ICESL models/20210725 pot/lion_planter.lua
+++ ICESL models/20210725 pot/lion_planter.lua
@@ -26,14 +26,13 @@ set_setting_value('cover_thickness_mm_0',1.2)
 
 sbox=load_centered_on_plate('pot.stl')
 -- 1.0 = small, 1.25 = medium, 1.5 = large
-sboxsize=2.0
+sboxsize=1.0
 sbox=scale(sboxsize)*sbox
-ss=bbox(sbox):extent()
 
 
 -- Base for planter
 
-function sbase(off,z)
+function sbase(off,z,ss)
   local w=2*off+ss.x
   local ws=2*off+ss.y-ss.x
   local base=cube(w,w,z)
@@ -49,13 +48,82 @@ function sbase(off,z)
   return base
 end
 
+ss=bbox(sbox):extent()
 halfthick=2.1
 gap=0.5
 base=difference(
-  sbase(halfthick+gap,2*halfthick),
-  translate(0,0,halfthick)*sbase(gap,halfthick)
+  sbase(halfthick+gap,2*halfthick,ss),
+  translate(0,0,halfthick)*sbase(gap,halfthick,ss)
 )
 -------------------------------------------------
+
+function spout(ws,h)
+  local r=4*ws/6
+  return intersection(
+        cube(ws,ws,h),
+        translate(0,-ws/2+r,0)*cylinder(r,h)
+      )
+end
+
+function flion(sc)
+  local lpot=load_centered_on_plate('pot.stl')
+  local l=intersection(lpot,
+    translate(0,-40,34)*cube(40,29,38))
+  l=scale(sc)*l
+  l=translate(-bbox(l):min_corner().x-bbox(l):extent().x/2,-bbox(l):max_corner().y,0)*l
+  return l
+end
+
+function leafy(sc)
+  local lpot=load_centered_on_plate('pot.stl')
+  local l=intersection(lpot,
+    translate(-28,-26,13)*cube(18,1,54))
+  l=scale(sc,4,sc)*l
+  l=translate(-bbox(l):min_corner().x-bbox(l):extent().x/2,-bbox(l):max_corner().y,0)*l
+  return l
+end   
+
+function newpot(sc)
+  local w=80*sc
+  local ws=30*sc
+  local h=80*sc
+  local hspout=35*sc
+  local wt=7
+  local base=difference(
+    cube(w,w,h),
+    translate(0,0,wt)*cube(w-wt,w-wt,h-wt))
+  
+  sp=difference(
+    translate(0,wt/2,0)*spout(ws,hspout),
+    translate(0,wt/2,wt)*spout(ws-wt,hspout-wt))
+
+  newp=union(
+    translate(0,ws/2,0)*base,
+    translate(0,-w/2,0)*
+    sp
+  )
+
+  local lion=translate(0,-25*sc,2)*flion(sc)
+  newp=union(newp,lion)
+
+  local leaf=translate(-28*sc,-24.5*sc,0)*leafy(sc)
+  leaf=union(leaf,mirror(X)*leaf)
+  newp=union(newp,leaf)
+  newp=difference(newp,translate(0,80*sc,0)*leaf)
+
+  newp=difference(newp,
+    union(
+    translate(0,-30,wt)*cube(20,20,10),
+    translate(0,-20,wt+10)*rotate(90,X)*cylinder(10,5)
+)
+)
+
+  return newp
+end
+
+newp=translate(0,100,0)*rotate(90,Z)*newpot(1.0)
+emit(newp)
+
 
 -- rotate so can print big
 
