8e79253a225356131526a74ef4e912e8ed50b15f
John <j842@users.noreply.github.com>
2021-09-19 18:16:50 +1200

    tidying

--- ICESL models/20210713 LolinNodeMCUv3/LolinNodeMCUv3_c.lua
+++ ICESL models/20210713 LolinNodeMCUv3/LolinNodeMCUv3_c.lua
@@ -1,3 +1,9 @@
+
+debug=true
+
+
+----
+
 package.path = package.path .. ";../common/?.lua"
 jshapes=require("jshapes")
 
@@ -6,9 +12,9 @@ set_setting_value('z_layer_height_first_layer_mm',0.3)
 set_setting_value('z_layer_height_mm',0.15)
 set_setting_value('gen_supports',true)
 set_setting_value('support_overhang_overlap_fraction',0.9)
-set_setting_value('infill_percentage_0',5)
+set_setting_value('infill_percentage_0',10)
 set_setting_value('infill_percentage_1',100) -- mount
-set_setting_value('infill_percentage_2',5)
+set_setting_value('infill_percentage_2',10)
 
 --------------------------------
 
@@ -185,14 +191,16 @@ function main()
   set_brush_color(1,0,1,0)
   emit(mnt,1)
 
-  local y=translate(0,0,0.8)*rotate(180,X)*load('NodeMcu_V3.stl')
-  y=translate(0,0,h)*y
-  set_brush_color (2,1,0,0)
-  --emit(y,2)
+  if (debug) then
+    local y=translate(0,0,0.8)*rotate(180,X)*load('NodeMcu_V3.stl')
+    y=translate(0,0,h)*y
+    set_brush_color (2,1,0,0)
+    emit(y,2)
+  end
 
-  local x=lolinv3template()
-  x=translate(0,0,h)*x
-  set_brush_color (3,1,0,1)
+  --local x=lolinv3template()
+  --x=translate(0,0,h)*x
+  --set_brush_color (3,1,0,1)
   --emit(x,3)
 end
 
