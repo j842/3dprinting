c7cfa433deff65ac620865fa1e50f12c098ad213
John <j842@users.noreply.github.com>
2021-10-02 21:56:35 +1300

    Update 20211002 Paul Dishwasher Wheel Mount.lua

--- ICESL models/20211002 Paul Dishwasher Wheel Mount.lua
+++ ICESL models/20211002 Paul Dishwasher Wheel Mount.lua
@@ -1,12 +1,19 @@
- -- Welcome to IceSL!
+-- Dishwasher replacement part for Paul.
 d1=6.3
 d2=8.3
 d3=12.95
 t=2.5
-l=15
+l=15.5
 
-cr=(d3-d1)/5
+--cr=(d3-d1)/5
 
+-- center in x and y, and place on z=0 surface.
+function xycenter(objtocenter)
+  local v=bbox(objtocenter):center()
+  objtocenter=translate(-v.x,-v.y,-v.z)*objtocenter
+  local h=bbox(objtocenter):min_corner().z
+  return translate(0,0,-h)*objtocenter
+end
 
 -- wheel clip/mount
 function wheelclip()
@@ -23,7 +30,7 @@ end
 
 -- bit rod clips in
 function rodclip()
-  local ph=d3/2+2
+  local ph=d3/2
   local p=difference(
   union(
   translate(0,d3/4,0)*cube(d3,ph,l),
@@ -34,24 +41,56 @@ function rodclip()
   p=difference(p,
     translate(0,ph/2,0)*cube(d1,ph,l-t))
 
-  local l2=l-d1-t
-  local cl=d3+0.5
-  local ctr=translate(-cl/2,d3/2-(d3-d1)/4,l2-cr-0.05)
-  local c=ctr*rotate(90,Y)*cylinder(cr,cl)
-  p=difference(p,c)
---emit(p)
   return p
 end
 
-function pin()
-  local p=cylinder(1,1)
+function springmechanism(kw,ke,kl1,kl,h)
+  local gap=0.5
+
+  local springer={ 
+      v{ke/2,0,0},
+      v{-ke/2,kl1-gap,0}, 
+      v{ke/2,kl1-gap,0},
+      v{3*ke/2,ke,0},
+      v{kw/2,kl1,0}, 
+      v{0,kl1,0},
+      v{0,kl,0},
+      v{kw,kl,0},
+      v{kw,0,0}}
+  local sm=linear_extrude(v(0,0,h),springer)
+  sm=difference(sm,
+    translate(0,kl1/2,h-gap)*
+    cube(kw/2,kl1,gap))
+  sm=rotate(90,X)*sm
+  sm=translate(-kw/2,h/2,0)*sm
+  return sm
 end
 
+function getbox(obj)
+  local bb=bbox(obj)
+  local c=cube(bb:extent())
+  c=translate(bb:center()-bbox(c):center())*c
+  return c
+end  
+
+h=(d3-d1)/2
+kw=(d3-d1)/2
+sm=springmechanism(kw,kw/3,l-t-d1,l,h)
+sm=translate(d3/2-kw/2,d3/2-h/2-0.05,0)*sm
+smb=getbox(sm)
+sm=union(sm,mirror(X)*sm)
+smb=union(smb,mirror(X)*smb)
+rc=rodclip()
+rc=difference(rc,smb)
+rc=union(rc,sm)
+
+emit(rc)
+
 p=union(
-translate(0,0,2*l)*rotate(180,Y)*rodclip(),
+translate(0,0,2*l)*rotate(180,Y)*rc,
 rotate(90,Z)*wheelclip())
 
-emit(rotate(90,X)*p)
+--emit(rotate(90,X)*p)
 
 
 
