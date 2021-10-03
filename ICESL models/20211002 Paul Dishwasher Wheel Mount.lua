3383ec947b676f5034a113a1d6a621eb7a4ed853
John <j842@users.noreply.github.com>
2021-10-03 17:29:26 +1300

    Update 20211002 Paul Dishwasher Wheel Mount.lua

--- ICESL models/20211002 Paul Dishwasher Wheel Mount.lua
+++ ICESL models/20211002 Paul Dishwasher Wheel Mount.lua
@@ -5,6 +5,7 @@ d3=12.95
 t=2.5
 l=15.5
 ph=d3/2
+wl=10.3+t+t
 
 --cr=(d3-d1)/5
 
@@ -21,9 +22,9 @@ function wheelclip()
   local o=difference(
     union(
       cylinder(10/2,2.5),
-      cylinder(d2/2,10.3+t+t)
+      cylinder(d2/2,wl)
     ),
-    cylinder(d1/2,10.3+t+t)
+    cylinder(d1/2,wl)
   )
   o=difference(o,cube(10,3,6))
   return o
@@ -58,38 +59,44 @@ function springmechanism(kw,ke,kl1,kl,h)
       v{kw,kl,0},
       v{kw,0,0}}
   local sm=linear_extrude(v(0,0,h),springer)
+
+  -- remove a piece so clip can move
   sm=difference(sm,
-    translate(0,kl1/2,h-gap)*
-    cube((kw+ke)/2,kl1,gap))
+    translate(5*ke/4-ke,kl1/2,h-gap)*
+    cube(5*ke/2,kl1,gap))
+
   sm=rotate(90,X)*sm
   sm=translate(-kw,0,0)*sm
   return sm
 end
 
-function getcube(obj,kw,kl,h)
+function getcube(obj)
   local bb=bbox(obj)
   local c=ccube(bb:extent())
---  local c=ccube(kw,h,kl)
   c=translate(bb:center())*c
   return c
 end  
 
-h=(d3-d1)/2
-kw=(d3-d1)/2
-sm=springmechanism(kw,kw/3,l-d1-t,l-t,h)
-sm=translate(d3/2,d3/2,0)*sm
--- this translate fixes some rounding error :/
-smb=translate(0,.2,0)*getcube(sm,kw,l,h)
-sm=union(sm,mirror(X)*sm)
-smb=union(smb,mirror(X)*smb)
-rc=rodclip()
-rc=difference(rc,smb)
-rc=union(rc,sm)
+function fullrodclip()
+  local h=(d3-d1)/2
+  local kw=(d3-d1)/2
+  local sm=springmechanism(kw,1.2,l-d1-t,l-t,h)
 
---emit(rc)
+  sm=translate(d3/2,d3/2,0)*sm
+  -- this translate fixes a gap, which 
+  -- maybe be incorrect code (or rounding)
+  smb=translate(0,.2,0)*getcube(sm,kw,l,h)
+  
+  sm=union(sm,mirror(X)*sm)
+  smb=union(smb,mirror(X)*smb)
+
+  return union(sm,difference(rodclip(),smb))
+end
+
+rc = fullrodclip()
 
 p=union(
-translate(0,0,2*l)*rotate(180,Y)*rc,
+translate(0,0,l+wl-t)*rotate(180,Y)*rc,
 rotate(90,Z)*wheelclip())
 
 emit(rotate(90,X)*p)
