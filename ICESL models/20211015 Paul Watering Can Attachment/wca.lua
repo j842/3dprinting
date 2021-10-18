c00b6491c37b19a1e4e6b0f780268f32b046b031
John <j842@users.noreply.github.com>
2021-10-18 21:26:00 +1300

    Update wca.lua

--- ICESL models/20211015 Paul Watering Can Attachment/wca.lua
+++ ICESL models/20211015 Paul Watering Can Attachment/wca.lua
@@ -7,44 +7,23 @@ hose=difference(
   cylinder(cyld/2,cyll)
 )
 
-asteps=10
+asteps=20
+pi=3.141592654
 
 contours={}
 for c=1,10 do
-  cc={}
+  contours[c]={}
   for a=1,asteps do
-    ar=a*180/asteps
-    table.insert(cc,
-      v(10*math.sin(ar),10*math.cos(ar),c))
+    ar=a*2*pi/asteps
+    contours[c][a]=
+      v(20*math.sin(ar),(c+5)*math.cos(ar),c)
   end
-
---   cc={}
---   table.insert(cc,v(0,0,c))
---   table.insert(cc,v(10,0,c))
---   table.insert(cc,v(10,10,c))
---   table.insert(cc,v(10*math.cos(c),10,c))
--- --  table.insert(contours,{v(1,1,c),v(1,0,c),v(0,1,c)})
---  contours[c]=cc
-  table.insert(contours,cc)
+   emit(translate(c,0,0)*linear_extrude(v(0,0,1), contours[c]))
 end
     
 
 se = sections_extrude(contours)
-emit(se)
-
--- se=rotate_extrude(
---   {v(0,0,0),v(0,5,3),v(0,5,5),v(0,2,5),v(0,0,0)},
---   100)
-
--- emit(se)
+--emit(se)
 
 
--- radius = v(10,0,0)
--- triangle = { radius + v(1,0,0), radius + v(0,1,0), radius + v(-1,0,0) }
--- emit( rotate_extrude( triangle, 100 ) )
-
-
---waterrr
-
- -- Welcome to IceSL!
 --emit(hose)
