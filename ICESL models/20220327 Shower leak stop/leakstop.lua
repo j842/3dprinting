9eefd3662451dafe78691d77cb2c6e11393d6db6
John <j842@users.noreply.github.com>
2022-04-01 19:20:22 +1300

    better

--- ICESL models/20220327 Shower leak stop/leakstop.lua
+++ ICESL models/20220327 Shower leak stop/leakstop.lua
@@ -10,7 +10,7 @@ set_setting_value('gen_supports',false)
 set_setting_value('infill_percentage_0',15)
 
 cr=6.7
-ov=20
+ov=10
 
 -- lose two lots of ov in the overlaps, so
 -- have to extend that much.
@@ -18,26 +18,24 @@ l=(620+2*ov)/3
 
 x=rotate(90,Y)*cylinder(cr,l)
 y=intersection(x,cube(2*l,2*l,l))
-y=difference(y,translate(0,cr/2,0)*cube(ov,cr,10))
+y=difference(y,translate(ov/2,cr/2,0)*cube(ov,cr,10))
 
-z=difference(y,translate(l,-cr/2,0)*cube(ov,cr,10))
+z=difference(y,translate(l-ov/2,-cr/2,0)*cube(ov,cr,10))
 
 deepz=3
 taperh=4.0
 r1=4.8/2
-r2=9.1/2
+
+holescale=9.1/8.2 -- at d=9.1, holes come out as 8.2 (actual, measured i3 Mk3s+ PETG)
+r2=(9.1*holescale)/2 
+
 screwhole=union(
 {
+  translate(0,0,cr-deepz)*cylinder(r2,deepz+0.001),
   translate(0,0,cr-taperh-deepz)*cone(r1,r2,v(0,0,0),v(0,0,taperh)),
-  translate(0,0,cr-deepz)*cylinder(r2,deepz+0.1),
   cylinder(r1,cr)
 }
 )
-
---z=difference(z,translate(l/2,0,cr-4.6)*
---cone(4.8/2,9.1/2,v(0,0,0),v(0,0,4.6)))
---z=difference(z,translate(l/2,0,0)*
---cylinder(4.8/2,cr))
 
 z=difference(z,translate(l/2,0,0)*screwhole)
 y=difference(y,translate(l-2*cr,0,0)*screwhole)
